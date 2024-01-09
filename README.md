1541dfc3a171cc71a72df3ce2970d8ff0e79faf3
lefv <lefv@c3.ai>
2024-01-09 12:20:53 -0500

    Added readme detail.

--- README.md
+++ README.md
@@ -1,2 +1,41 @@
 # c3sh
 ishell cli client for c3 server
+
+## overview
+
+c3 client Python CLI.
+Usage:
+
+'c3sh <subject> <action> <parameters>'
+
+command `help` accepted at any level.
+
+### structure:
+```
+|--c3sh.py
+|--subjects/
+|---------/*.py
+```
+### adding new command as `subject`
+```
+k8s_coredns = cli.subject(
+    name='k8s-coredns',
+    help='<one line description',
+    description='<detailed description of the subject>',
+    common_options=[
+        CliOption(['-c', '--context'], {'help': 'K8s context to use. If not specified current context will be used'})
+        <list of options that would be added to all subject actions>
+    ]
+)
+
+@k8s_coredns.command()
+def info(context=None):
+    '''Help message of info action'''
+
+
+@k8s_coredns.command(options=[
+    CliOption(['host'], {'help': 'Host name that need to be resolved'}),
+])
+def rewrite(host, context=None):
+    '''Help message for rewrite action'''
+```
--- c3sh.py
+++ c3sh.py
@@ -7,42 +7,7 @@
 # This material may be covered by one or more patents or pending patent applications.
 
 """
-c3 client Python CLI.
-Usage:
-'c3sh <subject> <action> <parameters>'
 
-command `help` accepted at any level.
-
-Structure:
-
-```
-|--c3sh.py
-|--subjects/
-|---------/*.py
-```
-
-```
-k8s_coredns = cli.subject(
-    name='k8s-coredns',
-    help='<one line description',
-    description='<detailed description of the subject>',
-    common_options=[
-        CliOption(['-c', '--context'], {'help': 'K8s context to use. If not specified current context will be used'})
-        <list of options that would be added to all subject actions>
-    ]
-)
-
-@k8s_coredns.command()
-def info(context=None):
-    '''Help message of info action'''
-
-
-@k8s_coredns.command(options=[
-    CliOption(['host'], {'help': 'Host name that need to be resolved'}),
-])
-def rewrite(host, context=None):
-    '''Help message for rewrite action'''
-```
 """
 
 
